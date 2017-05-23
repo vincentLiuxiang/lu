26eb9933823eb87ba4e11cf841ca6fd714f4d86c
vincentLiuxiang <hitliuyaguang@foxmail.com>
2017-05-23 13:37:00 +0800

    add: cookie / session example

---
+++ example/server.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"lu/example/controller"
+	"lu/example/lib"
+
+	"github.com/lugolang/static"
+
+	"github.com/vincentLiuxiang/lu"
+)
+
+func main() {
+	app := lu.New()
+	redis := lib.NewRedis("tcp", "localhost:6379", 60*60)
+	sessionMw := controller.NewSessionMiddleware("sid")
+	loginMw := controller.NewLoginMiddleware("sid")
+	fs := static.DefaultFS
+	Static := static.New(fs)
+
+	app.Use("/static/login", Static)
+	app.Post("/login", loginMw.Check(redis))
+	app.Use("/", sessionMw.Middleware(redis))
+	app.Use("/", loginMw.Middleware())
+	app.Use("/static/home", Static)
+
+	app.Listen(":8080")
+}
